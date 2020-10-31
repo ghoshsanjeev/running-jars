2007315a2d4e52b453e92454c76cfc38fd03aef0
Sanjeev <dgraymatter@gmail.com>
2020-11-01 02:27:02 +0530

    Added PPID, Path and formatting
    not using the padding logic

--- running-jars.sh
+++ running-jars.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 
-sudo netstat -ntlp |grep java | sed 1,2d > /tmp/output_netstat.txt
+sudo netstat -ntlp |grep java|tr -s " "|cut -d " " -f4,7|egrep -o "[0-9][0-9]+[[:space:]][0-9][0-9]+"|uniq > /tmp/netstat.out
 
-echo PORT$'\t'PID$'\t'NAME
-for port in $(cat /tmp/output_netstat.txt | awk '{print $4 " " $7}' | sed -e 's/.*://' | awk '{print $1}' | uniq)
+#Not using the padding logic
+#padlimit=20
+#pad=$(printf '%*s' "$padlimit")
+#pad=${pad// /-}
+#padlength=10
+
+printf '\n%6s %-40s%6s %6s  %-50s\n' "PORT" "NAME" "PID" "PPID" "PATH"
+echo "------------------------------------------------------------------------------------------------------------------------"
+for port in $(cat /tmp/netstat.out|awk '{print $1}')
 do
-    pid=$(cat /tmp/output_netstat.txt | grep -w "$port" | awk '{print $7}' | cut -d ' ' -f 7 | cut -d '/' -f 1 | uniq )
-    #ps_name=$(cat /tmp/output_netstat.txt | awk '{print $4 " " $7}' | sed -e 's/.*://' | sed 's/\// /g' | awk '{print $3}')
-    ps_name=$(ps -ef | grep "$pid" |egrep -o "(\w*[-])+[0-9]+([.][0-9]+)+[-]*(SNAPSHOT)*[.]jar")
-    #ps_name_output=$(ps -ef | grep "$pid" | grep "$ps_name" | grep -v grep |tr -s ' '| sed 's/^[^0-9]*//g' | head -1 | cut -f2- -d/)
-    echo  "$port"$'\t'"$pid"$'\t'"$ps_name
+    pid=$(cat /tmp/netstat.out |grep "$port"|cut -d ' ' -f2 )
+    ppid=$(ps -p "$pid" -o ppid=)
+    ps_name=$(ps -ef | grep "$pid" |egrep -o "(\w*[-]*)+([0-9]+([.][0-9]+)+)*[-]*(SNAPSHOT)*[.]jar")
+    path=$(sudo pwdx "$pid"|cut -d ':' -f2)
+    printf '%6d %-40s' "$port" "$ps_name"
+    #printf '%*.*s' 0 $((padlength - ${#ps_name})) "$pad"
+    printf '%6d %6d %-50s\n' "$pid" "$ppid" "$path"
+    #echo  "$port"$'\t'"$ps_name"$'\t\t\t'"$path"
 done
-
-rm -rf /tmp/output_netstat.txt
+echo "------------------------------------------------------------------------------------------------------------------------"
+echo
+rm -rf /tmp/netstat.out
